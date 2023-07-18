384053a88f7b7d7f3e1e8dc233f6ed2c8dddc844
Ajit malik <93873197+blackhat2548@users.noreply.github.com>
2023-07-18 19:24:34 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import Navbar from './components/Navbar/Navbar';
+import Menu from './components/Menu/Menu'
+import AnimatedRoutes from "./components/AnimatedRoutes/AnimatedRoutes";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Menu />
+      <AnimatedRoutes />
+    </div>
+  );
+}
+
+export default App;
